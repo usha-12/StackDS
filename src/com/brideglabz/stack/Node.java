cbf07f1735adee10bad523fae21fe3fdfa0da742
usha-12 <usha12ahirwar@gmail.com>
2022-11-04 01:37:52 +0530

    Create stack

---
+++ src/com/brideglabz/stack/Node.java
@@ -0,0 +1,28 @@
+package com.brideglabz.stack;
+
+public class Node <T>{
+    private T key;
+    private Node<T> next;
+
+    public Node(T key) {
+        this.key = key;
+    }
+
+    public T getKey() {
+        return key;
+    }
+
+    public void setKey(T key) {
+        this.key = key;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+}
+
+
